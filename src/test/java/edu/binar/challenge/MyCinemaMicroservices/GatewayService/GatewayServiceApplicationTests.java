df6a2092e9c77827ebafb1ab69be6a34bd5a32dc
mfyasykur <mf.yasykur@gmail.com>
2022-12-04 09:30:30 +0700

    initial commit GatewayService of MyCinema-Microservices

---
+++ src/test/java/edu/binar/challenge/MyCinemaMicroservices/GatewayService/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.binar.challenge.MyCinemaMicroservices.GatewayService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
